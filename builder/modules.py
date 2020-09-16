07d5239658fad8ee423d4d0276106a707e810cba
Slluxx <Slluxx@users.noreply.github.com>
2020-09-16 02:12:51 +0200

    Revert "Update modules.py"
    This reverts commit 415d593e4a95c9a6f295d4f8fb8cdd18b0b2411d.

--- builder/modules.py
+++ builder/modules.py
@@ -600,7 +600,7 @@ def download_awoo(module, temp_directory, deepsea_version, parameters, deepsea_b
     return get_version(module, release, 0)
 
 
-def download_nsainstaller(module, temp_directory, deepsea_version, parameters, deepsea_build):
+def download_nsxinstaller(module, temp_directory, deepsea_version, parameters, deepsea_build):
     release = get_latest_release(module)
     app_path = download_asset(module, release, 0)
     if app_path is None:
